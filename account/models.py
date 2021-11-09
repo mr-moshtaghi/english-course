114607209727bedb6f9522b5d356e2d444bb2260
mr-moshtaghi <mr.ahmadalimoshtaghi@gamil.com>
2021-11-09 22:02:07 +0330

    create model VerificationCode

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class VerificationCode(BaseModel):
+    code = models.CharField(max_length=20)
+    cellphone = models.CharField(max_length=20, null=True, blank=True)
+    expire_time = models.DateTimeField(null=True, blank=True)
+    last_send = models.DateTimeField(null=True, blank=True)
