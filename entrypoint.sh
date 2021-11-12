50064ec31315b8945019effab54fcfe5c92c9a38
mr-moshtaghi <mr.ahmadalimoshtaghi@gamil.com>
2021-11-13 00:00:17 +0330

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 manage.py collectstatic --noinput && python3 manage.py migrate
-uwsgi --plugins=python3 --chdir=/var/www --socket=0.0.0.0:9000 --module=khanebazia.wsgi:application --processes=4 --protocol=http --static-map /media=/var/www/media --static-map /static=/var/www/static
+uwsgi --plugins=python3 --chdir=/var/www --socket=0.0.0.0:9000 --module=english_course.wsgi:application --processes=4 --protocol=http --static-map /media=/var/www/media --static-map /static=/var/www/static
