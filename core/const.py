4742ad8b3f0d9507006acc67b82bcc4b88dab5a3
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2021-12-10 18:54:32 +0330

    create const for word types

--- core/const.py
+++ core/const.py
@@ -5,3 +5,15 @@ WORD_USER_STATUS_CHOICES = (
     (WORD_USER_STATUS_LEARNED, 'Learned'),
     (WORD_USER_STATUS_NOT_LEARNED, 'Not Learned'),
 )
+
+WORD_TYPE_ONE = 'type-one'
+WORD_TYPE_TWO = 'type-two'
+WORD_TYPE_THREE = 'type-three'
+WORD_TYPE_FOUR = 'type-four'
+
+WORD_TYPES = (
+    (WORD_TYPE_ONE, 'Type One'),
+    (WORD_TYPE_TWO, 'Type Two'),
+    (WORD_TYPE_THREE, 'Type Three'),
+    (WORD_TYPE_FOUR, 'Type Four'),
+)
