e0a4d0bcc68aee69e828c7ac1e60c8992a7025f6
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2021-12-12 19:20:04 +0330

    ops

--- english_course/urls.py
+++ english_course/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('api/v1/core/', include('core.urls', namespace='core'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
